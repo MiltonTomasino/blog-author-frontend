304e700058d4302b70513aac89dce7417cf1cd22
MiltonTomasino <jonathantomasino13@gmail.com>
2025-09-04 14:01:09 -0700

    Create Basic Skeleton and Server Connection

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const indexRoutes = require('./routes/index');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use("/", indexRoutes);
+
+app.listen(3001, ()=> {
+    console.log('Server is running on port 3001');
+})
