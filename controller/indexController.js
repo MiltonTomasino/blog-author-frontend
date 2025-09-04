5607d5b04efa24d948ea644824fbb99a11310918
MiltonTomasino <jonathantomasino13@gmail.com>
2025-09-04 14:26:24 -0700

    Pull Blogs From API and Render on Index Page

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,3 +1,8 @@
-module.exports.getIndexPage = (req, res) => {
-    res.render("index", { title: "Home Page" });
+module.exports.getIndexPage = async (req, res) => {
+
+    const blogs = await fetch("http://localhost:3000/blog")
+        .then(res => res.json())
+        .catch(err => console.error("Error fetching blogs:", err));
+    // console.log(blogs);
+    res.render("index", { title: "Home Page", blogs: blogs });
 }
